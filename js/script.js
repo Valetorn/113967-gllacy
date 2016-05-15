d89b102c17a29d846b518dd88714673d8a311763
Valetorn <rock_n_vladok@mail.ru>
2016-05-15 18:48:50 +0400

    Завершил верстку

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+document.getElementById('feedback-btn').addEventListener('click', function(e) {
+   e.preventDefault();
+}, false);
 function show(state) {
     document.getElementById('wrap-feedback').style.display = state;
     document.getElementById('feedback').style.display = state;
